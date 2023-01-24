9b8198f58e3cfbe975dbca8042f2f66a3ae026a0
SHASHWATSINGH20k <113877476+SHASHWATSINGH20k@users.noreply.github.com>
2023-01-24 15:24:33 +0530

    this is a phonebook

---
+++ Phonebook.c
@@ -0,0 +1,151 @@
+#include<stdio.h>
+#include<windows.h>
+void add_contact();
+void search_contact();
+void delete_contact();
+void view_all_contact();
+void count_contact();
+void main()
+{
+    printf("\n\t**PHONE BOOK**\n");
+    printf("\t 1. Add Contact\n");
+    printf("\t 2. Search Contact\n");
+    printf("\t 3. Delete Contact\n");
+    printf("\t 4. View All Contact\n");
+    printf("\t 5. Count Contact\n");
+    printf("\t 6. Exit\n");
+    printf("\tEnter Your Choice:-");
+    int option;
+    scanf("%d",&option);
+    switch(option)
+    {
+    case 1:
+        add_contact();
+        break;
+    case 2:
+        search_contact();
+        break;
+    case 3:
+        delete_contact();
+        break;
+    case 4:
+        view_all_contact();
+        break;
+    case 5:
+        count_contact();
+        break;    
+    case 6:
+        return;
+    default:
+        main();
+    }
+    return;
+}
+void add_contact()
+{
+    FILE *fp;
+    fp=fopen("contact.txt","a+");
+ 
+    printf("\t**ADD CONTACT**");
+    printf("\n\tName    :-");
+    char name[20];
+    scanf("%s",name);
+    printf("\n\tMob No. :-" );
+    char mob[20];
+    scanf("%s",mob);
+    fprintf(fp,"%s %s\n",name,mob);
+    fclose(fp);
+    printf("\n\t!!! File Saved !!!\n");
+    main();
+}
+void search_contact()
+{
+    FILE *fp;
+    fp=fopen("contact.txt","r");
+  
+    printf("\t**SEARCH CONTACT**");
+    printf("\n\t Enter Name :-");
+    char name[20];
+    scanf("%s",name);
+    char name1[20],mob[20];
+    while(fscanf(fp,"%s %s",name1,mob)!=EOF)
+    {
+        if(strcmp(name,name1)==0)
+        {
+            printf("\n\tNAME     :- %s\n",name1);
+            printf("\n\tMOB NO.  :- %s\n",mob);
+        }
+    }
+    fclose(fp);
+     printf("\n\t!!! File Saved !!!\n");
+    main();
+   
+}
+void delete_contact()
+{
+    FILE *fp,*fp1;
+    fp=fopen("contact.txt","r+");
+    fp1=fopen("temp.txt","w");
+    
+    printf("\t**DELETE CONTACT**");
+    printf("\n\t Enter Name :-");
+    char name[20];
+    scanf("%s",name);
+    char name1[20],mob[20];
+    while(fscanf(fp,"%s %s",name1,mob)!=EOF)
+    {
+        if(strcmp(name,name1)==0)
+        {
+            continue;
+        }
+        fprintf(fp1,"%s %s\n",name1,mob);
+    }
+    fclose(fp);
+    fclose(fp1);
+    fp=fopen("contact.txt","w");
+    fp1=fopen("temp.txt","r");
+    while(fscanf(fp1,"%s %s",name1,mob)!=EOF)
+    {
+
+        fprintf(fp,"%s %s\n",name1,mob);
+    }
+    fclose(fp);
+    fclose(fp1);
+    remove("temp.txt");
+   printf("\n\t!!! File Saved !!!\n");
+    main();
+}
+void view_all_contact()
+{
+    FILE *fp;
+    fp=fopen("contact.txt","r");
+ 
+    printf("\t**ALL CONTACTS**");
+    char name1[20],mob[20];
+    while(fscanf(fp,"%s %s",name1,mob)!=EOF)
+    {
+        printf("\n\t NAME    :-  %s",name1);
+        printf("\n\t MOB NO. :-  %s",mob);
+    }
+    fclose(fp);
+      printf("\n\t!!! File Saved !!!\n");
+  
+      main();
+}
+void count_contact()
+{
+    FILE *fp;
+    fp=fopen("contact.txt","r");
+  
+    printf("\t**ALL CONTACTS**");
+    int c=0;
+    char name1[20],mob[20];
+    while(fscanf(fp,"%s %s",name1,mob)!=EOF)
+    {
+       c++;
+    }
+    printf("\nNo. of Contacts = %d",c);
+    fclose(fp);
+    printf("\n\t!!! File Saved !!!\n");
+    main();
+}
